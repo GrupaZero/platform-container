13970c1bb8f140e7f56758b80c0d0b6b707c1379
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-12-27 15:06:49 +0100

    Added $OVERRIDE_UMASK

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-platform
 Docker container for platform
+
+## Available env variables:
+  - $OVERRIDE_UMASK
--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+function die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+if [ $OVERRIDE_UMASK ]; then
+    echo $OVERRIDE_UMASK | grep -E -q '^[0-7]{3}$' || die "Not valid umask"
+    umask $OVERRIDE_UMASK
+    echo -e "umask \e[91m$OVERRIDE_UMASK\e[0m"
+else
+    echo -e "umask \e[91m022\e[0m"
+fi
+
 /usr/bin/supervisord -n -c /etc/supervisord.conf
